24bed0e688a6bc3a0135877da07329ce69930bda
Melanija Racic <racicmelanija@gmail.com>
2022-04-12 14:46:34 +0200

    feat: Authenticate and authorize users with keycloak idp

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from 'src/utils/app.guard';
+import { DashboardComponent } from './dashboard/dashboard/dashboard.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: DashboardComponent, canActivate: [AuthGuard]},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
