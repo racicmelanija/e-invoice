f56677fe534af7e93f3fb56589ccbe60827b9ece
Melanija Racic <racicmelanija@gmail.com>
2022-05-04 10:44:08 +0200

    fix: Change authorized role in header

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -7,7 +7,7 @@ import { MatDialogModule } from "@angular/material/dialog";
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { AdminHeaderComponent } from './shared/layout/headers/admin-header/admin-header.component';
+import { HeaderComponent } from './shared/layout/headers/header/header.component';
 import { UsersComponent } from './admin/pages/users/users.component';
 import { PrimaryButtonComponent } from './shared/components/primary-button/primary-button.component';
 import { SecondaryButtonComponent } from './shared/components/secondary-button/secondary-button.component';
@@ -21,7 +21,7 @@ import { ReactiveFormsModule } from '@angular/forms';
 @NgModule({
   declarations: [
     AppComponent,
-    AdminHeaderComponent,
+    HeaderComponent,
     UsersComponent,
     PrimaryButtonComponent,
     SecondaryButtonComponent,
